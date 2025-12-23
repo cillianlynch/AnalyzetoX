d32c84e7686e885280d81fcf0a5c3c315f323c42
Cillian Lynch <lynchcillian@gmail.com>
2025-12-23 12:08:08 +0000

    Add PWA support

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then(() => console.log("Service Worker registered"))
+        .catch((err) => console.log("SW registration failed:", err));
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* PWA Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Theme color */}
+        <meta name="theme-color" content="#000000" />
+
+        {/* Apple support */}
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="apple-mobile-web-app-title" content="Analyzer" />
+
+      </Head>
       <body>
         <Main />
         <NextScript />
